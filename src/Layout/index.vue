06bbf2d72de4e4da0f1085b1704b674e2351f04a
spirit <guohuiying0714@163.com>
2023-04-06 20:05:56 +0800

    20230406 - B站 -【20.Layout整体布局】

--- src/Layout/index.vue
+++ src/Layout/index.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
-    Layout
+    <Sidebar></Sidebar>
+    <div>
+      <div>
+        <Navbar></Navbar>
+        <TagsView></TagsView>
+      </div>
+      <AppMain></AppMain>
+    </div>
   </div>
 </template>
 
 <script>
+import Sidebar from './components/Sidebar/index.vue'
+import TagsView from './components/TagsView/index.vue'
+import AppMain from  './components/AppMain.vue'
+import Navbar from './components/Navbar.vue'
 export default {
-  name:'Layout'
+  name:'Layout',
+  components: {
+    Navbar,
+    AppMain,
+    TagsView,
+    Sidebar
+  }
 }
 </script>
 
